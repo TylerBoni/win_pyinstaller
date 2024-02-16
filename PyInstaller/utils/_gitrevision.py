4ed8a23a79973fd7a45a31ca3593fe12f32efb8f
tylerboni <tboni@abonitech.com>
2024-02-16 10:35:42 -0800

    initial commit

--- PyInstaller/utils/_gitrevision.py
+++ PyInstaller/utils/_gitrevision.py
@@ -2,9 +2,10 @@
 # The content of this file will be filled in with meaningful data when creating an archive using `git archive` or by
 # downloading an archive from github, e.g., from github.com/.../archive/develop.zip
 #
-rev = "$Format:%h$"  # abbreviated commit hash
-commit = "$Format:%H$"  # commit hash
-date = "$Format:%ci$"  # commit date
-author = "$Format:%an$ <$Format:%ae$>"
-ref_names = "$Format:%D$"  # incl. current branch
-commit_message = """$Format:%B$"""
+rev = "90256f93ed"  # abbreviated commit hash
+commit = "90256f93ed943daf6de53c7dd39710a415f705cb"  # commit hash
+date = "2024-02-10 13:47:22 +0000"  # commit date
+author = "Brénainn Woodsend <bwoodsend@gmail.com>"
+ref_names = "tag: v6.4.0"  # incl. current branch
+commit_message = """Release v6.4.0. [skip ci]
+"""
